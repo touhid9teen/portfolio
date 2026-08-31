bf1a60ae649f2ab070660823967a3179832db833
touhid <touhidul.islam@upaybd.com>
2026-08-31 11:09:58 +0600

    feat: add animated splash cursor effect

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,6 +4,7 @@ import BlogPosts from "../features/blog/components/BlogPosts";
 import GalaxyBackdrop from "../features/home/components/GalaxyBackdrop";
 import HeroCard from "../features/home/components/HeroCard";
 import Navbar from "../shared/components/Navbar";
+import SplashCursor from "../shared/components/SplashCursor";
 import useSceneController from "./hooks/useSceneController";
 import { getBlogStageClass, getHeroStageClass, getNavStageClass } from "./utils/sceneClasses";
 
@@ -12,6 +13,18 @@ function App() {
 
   return (
     <div className="relative isolate min-h-screen">
+      <SplashCursor
+        DENSITY_DISSIPATION={3.5}
+        VELOCITY_DISSIPATION={2}
+        PRESSURE={0.1}
+        CURL={3}
+        SPLAT_RADIUS={0.2}
+        SPLAT_FORCE={6000}
+        COLOR_UPDATE_SPEED={10}
+        SHADING
+        RAINBOW_MODE={false}
+        COLOR="#A855F7"
+      />
       <GalaxyBackdrop />
       <div
         className={`relative z-10 mx-auto max-w-3xl px-6 py-12 max-sm:px-4 max-sm:py-6 ${
